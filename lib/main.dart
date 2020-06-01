d76a8b4338d94b59c371ebf6c0ac3191e1ad9d97
gustavo <gustavo.maia@b2b.com.pt>
2020-06-01 14:45:19 +0100

    Ajustando o visual do Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,73 @@ class ByteBankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      home: Scaffold(
-        appBar: AppBar(title: Text("Dashboard"),),
-        
-        body: Column(
-          children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              height :120,
-              width: 100,
-              color: Colors.green,
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+
+      home: Dashboard()
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Dashboard"),),
+
+      body: Column(
+
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FittedBox(
+                    child: Image.asset('images/bytebank_logo.png'),
+                    fit: BoxFit.fill,
+                  )
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height :100,
+              width: 150,
+              color: Theme.of(context).primaryColor,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.people),
-                  Text("Contacts")
+                  Icon(Icons.people,
+                    color: Colors.white,
+                    size: 24.0 ,
+                  ),
+                  Text("Contacts",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ))
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
 
 
 
