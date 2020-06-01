470869667aaf313a4d5f796873e1f403f5cc5b24
gustavo <gustavo.maia@b2b.com.pt>
2020-06-01 15:29:23 +0100

    Implementando a lista de contatos

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bytebankofficial/screens/contacts_list.dart';
+import 'package:bytebankofficial/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,60 +25,5 @@ class ByteBankApp extends StatelessWidget {
     );
   }
 }
-
-class Dashboard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Dashboard"),),
-
-      body: Column(
-
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: FittedBox(
-                    child: Image.asset('images/bytebank_logo.png'),
-                    fit: BoxFit.fill,
-                  )
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              height :100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.people,
-                    color: Colors.white,
-                    size: 24.0 ,
-                  ),
-                  Text("Contacts",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      ))
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
 
 
