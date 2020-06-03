559bcc64eae390474650bc963ee86aef61d830dd
gustavo <gustavo.maia@b2b.com.pt>
2020-06-03 09:58:18 +0100

    Utilizando o async await

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,8 +2,25 @@ import 'package:bytebankofficial/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase(){
+Future<Database> getDatabase() async{
 
+  final String path = join(await getDatabasesPath(), "bytebank.db");
+
+  return openDatabase(path,
+
+    onCreate: (db, version) {
+
+      db.execute("CREATE TABLE contacts("
+          "id INTEGER PRIMARY KEY, "
+          "name TEXT, "
+          "account_number INTEGER)");
+
+    },
+    version: 1,
+    //onDowngrade: onDatabaseDowngradeDelete
+  );
+
+  /*
   return getDatabasesPath().then((dbPath) {
 
     final String path = join(dbPath, "bytebank.db");
@@ -20,12 +37,23 @@ Future<Database> createDatabase(){
         //onDowngrade: onDatabaseDowngradeDelete
     );
   });
+  */
 }
 
 
-Future<int> save(Contact contact){
+Future<int> save(Contact contact) async{
 
-  return createDatabase().then((db) {
+  final Database db = await getDatabase();
+
+  final Map<String, dynamic> contactMap = Map();
+
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
+
+  return db.insert('contacts', contactMap);
+
+  /*
+  return getDatabase().then((db) {
 
     final Map<String, dynamic> contactMap = Map();
 
@@ -34,11 +62,26 @@ Future<int> save(Contact contact){
 
     return db.insert('contacts', contactMap);
   });
+  */
 }
 
-Future<List<Contact>> findAll(){
+Future<List<Contact>> findAll() async{
 
-  return createDatabase().then((db) {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+
+  final List<Contact> contacts = List();
+
+  for(Map<String, dynamic> row in result){
+
+    final Contact contact = Contact(row['id'], row['name'], row['account_number']);
+    contacts.add(contact);
+  }
+
+  return contacts;
+
+  /*
+  return getDatabase().then((db) {
     return db.query('contacts').then((maps){
 
       final List<Contact> contacts = List();
@@ -52,5 +95,5 @@ Future<List<Contact>> findAll(){
       return contacts;
     });
   });
-
+*/
 }
