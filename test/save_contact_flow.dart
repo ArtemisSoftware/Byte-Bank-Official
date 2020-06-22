49d5ff4a6b2323775048f2224df20ee630f43027
gustavo <gustavo.maia@b2b.com.pt>
2020-06-22 15:17:13 +0100

    Verificando chamadas do banco de dados

--- test/save_contact_flow.dart
+++ test/save_contact_flow.dart
@@ -1,9 +1,11 @@
 import 'package:bytebankofficial/main.dart';
+import 'package:bytebankofficial/models/contact.dart';
 import 'package:bytebankofficial/screens/contact_form.dart';
 import 'package:bytebankofficial/screens/contacts_list.dart';
 import 'package:bytebankofficial/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import 'matchers.dart';
 import 'mocks.dart';
@@ -32,10 +34,14 @@ void main(){
       final contactsList = find.byType(ContactsList);
       expect(contactsList, findsOneWidget);
 
+
+      verify(mockContactDao.findAll()).called(1);
+
+
+
       final fabNewContact = find.widgetWithIcon(FloatingActionButton, Icons.add);
       expect(fabNewContact, findsOneWidget);
       await tester.tap(fabNewContact);
-
       await tester.pumpAndSettle();
 
 
@@ -45,11 +51,7 @@ void main(){
 
       final nameTextField = find.byWidgetPredicate((widget)  {
 
-          if(widget is TextField){
-              return widget.decoration.labelText == "Full name";
-          }
-
-          return false;
+          return _textFieldMatcher(widget, "Full name");
       });
 
       expect(nameTextField, findsOneWidget);
@@ -59,11 +61,7 @@ void main(){
 
       final accountNumberTextField = find.byWidgetPredicate((widget)  {
 
-          if(widget is TextField){
-              return widget.decoration.labelText == "Account number";
-          }
-
-          return false;
+          return _textFieldMatcher(widget, "Account number");
       });
 
       expect(accountNumberTextField, findsOneWidget);
@@ -72,13 +70,23 @@ void main(){
 
       final createButton = find.widgetWithText(RaisedButton, 'Create');
       expect(createButton, findsOneWidget);
-
       await tester.tap(createButton);
       await tester.pumpAndSettle();
+      
+      verify(mockContactDao.save(Contact(0, "Alex", 1000)));
 
       final contactsListBack = find.byType(ContactsList);
       expect(contactsListBack, findsOneWidget);
 
 
+      verify(mockContactDao.findAll());
   });
+}
+
+bool _textFieldMatcher(Widget widget, String labelText) {
+  if(widget is TextField){
+      return widget.decoration.labelText == labelText;
+  }
+
+  return false;
 }
